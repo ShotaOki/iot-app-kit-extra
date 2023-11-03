e8d47b8cfb783ae30effff517703564e4bb2de95
Shota Oki <oki.08031367557@i.softbank.jp>
2023-11-03 13:13:17 +0900

    dev実行時に文字化けを起こす不具合を修正

--- examples/typescript-vite-simple/src/main.tsx
+++ examples/typescript-vite-simple/src/main.tsx
@@ -6,6 +6,11 @@ import "./index.css";
 /// <reference types="vite/client" />
 
 window.global = window.global || {};
+if (window.global.TextDecoder === undefined) {
+  // arraybuffer-to-stringの判定のために必要
+  // global.TextDecoderからTextDecoderが参照できないと文字化けを起こすため
+  window.global.TextDecoder = TextDecoder;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
